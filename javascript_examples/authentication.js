0d5499de80f402c73562f94ea653365674b6741f
Goran Iliev <goraniliev93@yahoo.com>
2016-11-17 17:48:40 +0100

    JavaScript authentication example added

---
+++ javascript_examples/authentication.js
@@ -0,0 +1,31 @@
+/**
+ * npm install -g crypto-js
+ * npm install -g request
+ */
+
+var crypto = require('crypto-js');
+
+var public_key = 'enter your public key';
+var secret_key = 'enter your secret key';
+var timestamp = Math.floor(Date.now() / 1000);
+var payload = timestamp + '.' + public_key;
+var hash = crypto.HmacSHA256(payload, secret_key);
+var hex_hash = crypto.enc.Hex.stringify(hash);
+var signature = payload + "." + hex_hash;
+var ticker_btcusd_url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
+
+var request = require('request');
+var options = {
+    url: ticker_btcusd_url,
+    headers: {
+        'X-Signature': signature
+    }
+};
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
+
