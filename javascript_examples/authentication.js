1a29da9c320f00bb0f2288efacf5696ae42c5c67
darko <kolevdarko@gmail.com>
2020-02-05 15:26:11 +0100

    updated javascript examples

--- javascript_examples/authentication.js
+++ javascript_examples/authentication.js
@@ -6,23 +6,17 @@
 var crypto = require('crypto-js');
 
 var public_key = 'enter your public key';
-var secret_key = 'enter your secret key';
-var timestamp = Math.floor(Date.now() / 1000);
-var payload = timestamp + '.' + public_key;
-var hash = crypto.HmacSHA256(payload, secret_key);
-var hex_hash = crypto.enc.Hex.stringify(hash);
-var signature = payload + "." + hex_hash;
 var ticker_btcusd_url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
 
 var request = require('request');
 var options = {
     url: ticker_btcusd_url,
     headers: {
-        'X-Signature': signature
+        'x-ba-key': public_key
     }
 };
 function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
         console.log(body);
     }
 }
